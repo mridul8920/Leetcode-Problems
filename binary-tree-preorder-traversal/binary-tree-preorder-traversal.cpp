829e87b2c1833a8eda289ebb6457ec4bbf13ce5c
Mridul Kapoor <66908728+mridul8920@users.noreply.github.com>
2021-12-30 18:47:52 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (74.58%) - LeetHub

---
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>v;
+        if(root==NULL)
+            return v;
+        stack<TreeNode*>st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode *cur=st.top();
+            st.pop();
+            v.push_back(cur->val);
+            if(cur->right) st.push(cur->right);
+            if(cur->left)   st.push(cur->left);
+            //KAHANI MAI TWIST AS WE ARE USING A STACK SO INSTEAD OF PUSHING THE LEFT SUB TREE                 WE ARE PUSHING THE RIGHT SUBTREE AS STACK FOLLOWS 'LIFO'.
+        }
+        return v;
+    }
+};
