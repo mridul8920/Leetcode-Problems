fd05b02e48c9c6efb89266f350130eb63017a960
Mridul Kapoor <66908728+mridul8920@users.noreply.github.com>
2022-02-08 00:47:30 +0530

    Added solution - LeetHub

---
+++ Excel Sheet | Part - 1 - GFG/excel-sheet-part-1.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+    string ExcelColumn(int N)
+    {
+        // Your code goes here
+        string result="";
+      while(N)
+      {
+          char c='A'+(N-1)%26;
+          result= c + result;
+          N = (N-1)/26;
+      }
+      return result;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        Solution ob;
+        cout<<ob.ExcelColumn(n)<<endl;
+    }
+    return 0;
+}
+      // } Driver Code Ends
