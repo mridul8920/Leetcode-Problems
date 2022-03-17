fc406bc43602c5dc699769090789be744a17eb09
Mridul Kapoor <66908728+mridul8920@users.noreply.github.com>
2022-03-17 21:26:15 +0530

    Time: 7 ms (70.64%), Space: 10.7 MB (68.54%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void findCombi(int ind,int target,vector<int>&arr,vector<int>&ds,vector<vector<int>>&ans){
+            if(target==0){
+                ans.push_back(ds);
+                return;
+            }
+    
+        for(int i=ind;i<arr.size();i++){
+        if(i>ind and arr[i]==arr[i-1]) continue;
+        if(arr[i]>target) break;
+        ds.push_back(arr[i]);
+        findCombi(i+1,target-arr[i],arr,ds,ans);
+        ds.pop_back();    
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+         vector<int>ds;
+        vector<vector<int>>ans;
+        sort(candidates.begin(),candidates.end());
+        findCombi(0,target,candidates,ds,ans);
+        return ans;
+    }
+};
