7e29954f2afe2138a53c86425fe70f8a370b1971
Mridul Kapoor <66908728+mridul8920@users.noreply.github.com>
2022-03-17 12:29:18 +0530

    Time: 4 ms (70.12%), Space: 7.5 MB (73.91%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void powerSet(vector<vector<int>>&v,vector<int>&nums,vector<int>vec,int begin){
+        v.push_back(vec);
+        for (int i = begin; i != nums.size(); ++i) {
+            if (i == begin || nums[i] != nums[i - 1]){
+			vec.push_back(nums[i]);
+			powerSet(v, nums, vec, i + 1);
+			vec.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>>v;
+        vector<int>temp;
+        sort(nums.begin(),nums.end());
+        powerSet(v,nums,temp,0);
+        return v;
+    }
+};
