4df6ea8d1edd43d2f4bc441774cb573939878d6e
Mridul Kapoor <66908728+mridul8920@users.noreply.github.com>
2022-06-27 20:29:48 +0530

    Time: 39 ms (39.83%), Space: 10.5 MB (90.96%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int t[1001][1001];
-    int lcs(string &x,string &y,int n,int m){
-        if(n==0 or m==0)
-            return 0;
-        if(t[n][m]!=-1)
-            return t[n][m];
-        if(x[n-1]==y[m-1])
-            return t[n][m]=1+lcs(x,y,n-1,m-1);
-        else
-            return t[n][m]=max(lcs(x,y,n-1,m),
-                       lcs(x,y,n,m-1));
+    int lcs(string &x,string &y,int m,int n){
+        //INITIALIZATION
+        for(int i=0;i<m+1;i++)
+            for(int j=0;j<n+1;j++)
+                 if(i==0 or j==0)
+                        t[i][j]= 0;
+        //  CHOICE DIAGRAM
+         for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(x[i-1]==y[j-1])
+                    t[i][j]=1+t[i-1][j-1];
+                else
+                    t[i][j]=max(t[i-1][j],t[i][j-1]);
+            }
+        }
+        return t[m][n];
+        
     }
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
