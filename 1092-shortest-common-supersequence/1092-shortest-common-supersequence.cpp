29840b607b55627b21843545a47633303f093b40
Mridul Kapoor <66908728+mridul8920@users.noreply.github.com>
2022-07-03 23:11:48 +0530

    Time: 44 ms (27.39%), Space: 10.8 MB (66.48%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,28 +1,52 @@
 class Solution {
 public:
-    string lcs(string& A, string& B) {
-        int n = A.size(), m = B.size();
-        vector<vector<string>> dp(n + 1, vector<string>(m + 1, ""));
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < m; ++j)
-                if (A[i] == B[j])
-                    dp[i + 1][j + 1] = dp[i][j] + A[i];
+    int t[1001][1001];
+    string LCS(string x,string y){
+        string s;
+        int m=x.size(),n=y.size();
+        for(int i=0;i<m+1;i++)
+            for(int j=0;j<n+1;j++)
+                if(i==0 or j==0)
+                    t[i][j]=0;
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(x[i-1]==y[j-1])
+                    t[i][j]=1+t[i-1][j-1];
                 else
-                    dp[i + 1][j + 1] = dp[i + 1][j].size() > dp[i][j + 1].size() ?  dp[i + 1][j] : dp[i][j + 1];
-        return dp[n][m];
-    }
-    string shortestCommonSupersequence(string A, string B) {
-        string res="";
-        int i=0,j=0;
-        for(auto c:lcs(A,B)){
-            while(A[i]!=c)
-                res+=A[i++];
-            while(B[j]!=c)
-                res+=B[j++];
-            res+=c;
-            i++;
-            j++;
+                    t[i][j]=max(t[i-1][j],t[i][j-1]);
+            }
         }
-        return res+A.substr(i)+B.substr(j);
+        int i=m,j=n;
+        while(i>0 and j>0){
+            if(x[i-1]==y[j-1]){
+                s.push_back(x[i-1]);
+                i--;
+                j--;
+            }
+            else{
+                if(t[i][j-1]>t[i-1][j]){
+                    s.push_back(y[j-1]);
+                    j--;
+                }
+                else{
+                    s.push_back(x[i-1]);
+                    i--;
+                }
+            }
+        }
+        while(i>0){
+             s.push_back(x[i-1]);
+                    i--;
+        }
+        while(j>0){
+            s.push_back(y[j-1]);
+                    j--;
+        }
+        reverse(s.begin(),s.end());
+        return s;
+    }
+    string shortestCommonSupersequence(string str1, string str2) {
+        memset(t,-1,sizeof(t));
+        return LCS(str1,str2);
     }
 };
