613111a3ab3b4e0001035484f772448898839d8f
Mridul Kapoor <66908728+mridul8920@users.noreply.github.com>
2022-10-26 20:58:25 +0530

    Time: 7 ms (60.01%), Space: 15.7 MB (21.52%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void solve(int open,int close,string op,vector<string>&v){
+    if(open==0 and close==0){
+        v.push_back(op);
+        return;
+    }
+    if(open!=0){
+        string op1=op;
+        op1.push_back('(');
+        solve(open-1,close,op1,v);
+    }
+    if(open<close){
+        string op2=op;
+        op2.push_back(')');
+        solve(open,close-1,op2,v);
+        return;
+    }
+    
+}
+
+    vector<string> generateParenthesis(int n) {
+            vector<string>v;
+    string op="";
+    int open=n,close=n;
+    solve(open,close,op,v);
+    return v;
+
+    }
+};
